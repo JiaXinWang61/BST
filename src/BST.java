0dad6af53419109724cc9fee6e7b9dafe2a75d18
JiaXinWang61 <584937529@qq.com>
2020-04-06 20:42:21 +0800

    first commit

---
+++ src/BST.java
@@ -0,0 +1,60 @@
+public class BST<E extends Comparable<E>> {
+
+    private class Node {
+        public E e;
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    public Node root;
+    public int size;
+
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    //向二分搜索树添加元素
+    public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        }else {
+            add(root,e);
+        }
+    }
+
+    //以node为根的二分搜索树添加元素
+    private void add(Node node, E e) {
+        if (e.equals(node.e)) {
+            return;
+        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        }
+        if (e.compareTo(node.e) < 0) {
+            add(node.left, e);
+        } else {
+            add(node.right, e);
+        }
+    }
+}
